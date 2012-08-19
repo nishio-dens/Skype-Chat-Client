f40d9b9b8369c863a499955dda87bc731de8439b
S.Nishio <nishio@densan-labs.net>
2012-08-19 17:31:59 +0900

    Ballon Notifyを追加

---
+++ SkypeChatClient/Baloon.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SKYPE4COMLib;
+using System.Windows.Forms;
+
+namespace SkypeChatClient
+{
+    public class Baloon : MessageReceivedListener
+    {
+        public NotifyIcon NotifyIcon { get; private set; }
+
+        public Baloon(NotifyIcon icon)
+        {
+            NotifyIcon = icon;
+        }
+
+        public void ActionWhenReceivedMessage(IChatMessage message)
+        {
+            NotifyIcon.BalloonTipTitle = message.Sender.Handle + "(" + message.Chat.FriendlyName + ")";
+            NotifyIcon.BalloonTipText = message.Body;
+            NotifyIcon.ShowBalloonTip(1000);
+        }
+    }
+}
