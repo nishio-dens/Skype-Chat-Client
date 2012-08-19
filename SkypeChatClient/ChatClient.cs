bbb0c657032249b1a8acd8d18b5de990d471f422
S.Nishio <nishio@densan-labs.net>
2012-08-19 15:21:57 +0900

    Attach関数を実装

--- SkypeChatClient/ChatClient.cs
+++ SkypeChatClient/ChatClient.cs
@@ -3,11 +3,50 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using SKYPE4COMLib;
+using AxSKYPE4COMLib;
+using System.Windows.Forms;
 
 namespace SkypeChatClient
 {
     public class ChatClient
     {
+        AxSkype SkypeClient { get; set; }
+
+        public ChatClient(AxSkype skype)
+        {
+            SkypeClient = skype;
+            AttachToSkypeClient();
+        }
+
+        /// <summary>
+        /// スカイプとの連携を開始します。
+        /// </summary>
+        /// <param name="skype"></param>
+        public void AttachToSkypeClient()
+        {
+            if (!SkypeClient.Client.IsRunning)
+            {
+                if (MessageBox.Show("Skypeを起動してもよろしいですか？", "Client", MessageBoxButtons.YesNo) == DialogResult.Yes)
+                {
+                    SkypeClient.Client.Start();
+                }
+                else
+                {
+                    return;
+                }
+            }
+            try
+            {
+                SkypeClient.Attach();
+            }
+            catch
+            {
+                MessageBox.Show(
+                    "Skypeと連携できませんでした。本プログラムからSkypeにアクセスできるよう許可してください。",
+                    "Error");
+            }
+        }
+
         /// <summary>
         /// 指定したチャットに指定したメッセージを送信します。
         /// </summary>
